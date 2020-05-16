4d2a025f8e5f88f03a6c8c22c15a9296261463b6
Cortney Thomas <cortney@Cortneys-MacBook-Pro.local>
2020-05-16 16:34:34 -0400

    add tabs

--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -1,20 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { PlacesPageRoutingModule } from './places-routing.module';
-
-import { PlacesPage } from './places.page';
+import { PlacesPageRoutingModule } from "./places-routing.module";
+import { PlacesPage } from "./places.page";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    PlacesPageRoutingModule
-  ],
-  declarations: [PlacesPage]
+  imports: [CommonModule, IonicModule, PlacesPageRoutingModule],
+  declarations: [PlacesPage],
 })
 export class PlacesPageModule {}
