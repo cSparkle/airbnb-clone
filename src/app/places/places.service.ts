39d80f74c8ce0c2c20542bf86ed3b041783a7348
Cortney Thomas <cortney@Cortneys-MBP.hsd1.mi.comcast.net>
2020-05-17 13:40:57 -0400

    add discover cards and items

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@angular/core";
+import { Place } from "./place.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      "p1",
+      "Manhattan Mansion",
+      "In the heart of NYC",
+      "../../assets/images/nyc_mansion.jpg",
+      489.99
+    ),
+    new Place(
+      "p2",
+      "L'Amour Toujour",
+      "A romantic hideaway in Paris",
+      "../../assets/images/paris.jpeg",
+      349.99
+    ),
+    new Place(
+      "p3",
+      "The Foggy Palace",
+      "Not your average hotel room!",
+      "../../assets/images/fairy_cottage.jpg",
+      149.99
+    ),
+  ];
+
+  // we do this so we can't directly edit the _places array
+  // from anywhere else where we might access the _places array
+  get places() {
+    return [...this._places];
+  }
+  constructor() {}
+}
