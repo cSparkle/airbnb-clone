13076c8638ea97f8cd8278265596151a1116e48e
Cortney Thomas <cortney@Cortneys-MBP.hsd1.mi.comcast.net>
2020-05-17 16:05:12 -0400

    add menu

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { PlacesService } from "../places.service";
 import { Place } from "../place.model";
+import { MenuController } from "@ionic/angular";
 
 @Component({
   selector: "app-discover",
@@ -10,9 +11,18 @@ import { Place } from "../place.model";
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
 
-  constructor(private placesService: PlacesService) {}
+  constructor(
+    private placesService: PlacesService,
+    private menuCtrl: MenuController
+  ) {}
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
   }
+
+  // leaving this here to show that you can access the menu controller to get all kinds of details about
+  // the menus in an app as well as perform operations
+  // onOpenMenu() {
+  //   this.menuCtrl.toggle();
+  // }
 }
